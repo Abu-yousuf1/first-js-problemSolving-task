2f0dee0d238cbb7bf3ac3735b383e263f4a0d9c1
abu-yousuf1 <yousufadnan111@gmail.com>
2021-08-05 18:24:22 +0600

    commit done

---
+++ recurtion.js
@@ -0,0 +1,11 @@
+function recursion(number) {
+    if (number == 0) {
+        return;
+    }
+    console.log(number);
+    debugger;
+    recursion(number - 1)
+}
+const myRecur = recursion(5);
+
+
